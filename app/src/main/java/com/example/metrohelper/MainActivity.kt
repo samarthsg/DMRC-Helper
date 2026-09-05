c862ed66a3b9b5bf68e917790dba24555bb4589c
Amit Shahare <165269001+aMIT-sHAHARE@users.noreply.github.com>
2026-09-05 20:30:13 +0530

    add RoutePlanner
    RoutePlanner

--- app/src/main/java/com/example/metrohelper/MainActivity.kt
+++ app/src/main/java/com/example/metrohelper/MainActivity.kt
@@ -43,6 +43,7 @@ import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.LocalParking
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Map
+import androidx.compose.material.icons.filled.DirectionsTransit//new
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Subway
 import androidx.compose.material.icons.filled.Train
@@ -166,6 +167,7 @@ fun MetroApp() {
         BottomNavItem("metromap", "Metro Map", Icons.Default.Map),
         BottomNavItem("neareststation", "Nearby", Icons.Default.LocationOn), // ✅ NEW
         BottomNavItem("booktickets", "Tickets", Icons.Default.ConfirmationNumber),
+        BottomNavItem("routeplanner", "Route", Icons.Default.DirectionsTransit),//NEW
         BottomNavItem("availableparkings", "Parkings", Icons.Default.LocalParking),
         BottomNavItem("aboutapp", "About App", Icons.Default.Info)
     )
@@ -300,7 +302,7 @@ fun NavigationGraph(navController: NavHostController) {
         composable("metromap") { MetroMapScreen() }
 
         composable("neareststation") { NearestStationScreen() } // ✅ NEW
-
+        composable("routeplanner") { RoutePlannerScreen() }//NEW
         composable("booktickets") { BookTicketsScreen() }
         composable("availableparkings") { AvailableParkingsScreen() }
         composable("aboutapp") { AboutAppScreen() }
